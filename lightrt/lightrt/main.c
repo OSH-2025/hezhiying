2701e8e35d2fa94151996d6b7b7ba95808e96e7d
Jiahong Luo <luojh@mail.ustc.edu.cn>
2025-07-03 15:38:47 +0000

    The final commit for the code part of this project in this period -- by luojh

--- lightrt/lightrt/main.c
+++ lightrt/lightrt/main.c
@@ -6,22 +6,27 @@
 int signalid;
 mxsram volatile uint32_t sharedvar;
 
-void mytask(void *arg)
+/* Code for feature test */
+void panic()
 {
-    /*while (1)
+    reset_handler();
+}
+
+void test_task1(void *arg)
+{
+    uart_printf("Task1 started. Argument = %x", arg);
+
+    while (1)
     {
-        // lrt_uart_write("Hello, world!\n");
+        lrt_uart_write("Hello, world!\n");
         lrt_signal_wait(signalid);
         uart_printf("Hello, world! Task 1 Current time: %d\n", lrt_clock_tick());
-    }*/
+    }
+}
 
-    lrt_uart_write("Mytask created.\n");
-    lrt_uart_write("Mytask created.\n");
-    lrt_uart_write("Mytask created.\n");
-    lrt_uart_write("Mytask created.\n");
-    lrt_uart_write("Mytask created.\n");
-
-    /* uint32_t src[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+void test_dma(void *arg)
+{
+    uint32_t src[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     uint32_t dst[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
     lrt_dma_init();
@@ -35,8 +40,14 @@ void mytask(void *arg)
     for (int i = 0; i < 10; ++i)
     {
         uart_printf("Copied %d -> %d\n", src[i], dst[i]);
-    } */
+    }
+}
 
+void test_uart()
+{
+    uart_printf("Testing UART print...\n");
+
+    uart_printf("Testing UART read. Please press 10 keys.\n");
     char buf[10];
     lrt_uart_read(buf, 10);
 
@@ -44,20 +55,40 @@ void mytask(void *arg)
     {
         uart_printf("%c = %d\n", buf[i], buf[i]);
     }
+}
+
+void test_task0(void *arg)
+{
+    lrt_uart_write("Task 0 created.\n");
 
     while (1)
         ;
 }
 
-void mytask2(void *arg)
+void test_task2(void *arg)
 {
-    /*while (1)
+    while (1)
     {
         lrt_clock_delay(20);
         // lrt_uart_write("Hello, world!\n");
         uart_printf("Hello, world! Task 2 Current time: %d\n", lrt_clock_tick());
         lrt_signal_active(signalid);
-    }*/
+    }
+}
+
+void test_exmem(void *arg)
+{
+    *maddr(0x60000008) = 1958;
+    uint32_t read_value = *maddr(0x60000008);
+    uart_printf("Read test: %d\n", read_value);
+
+    uart_printf("exmem lock = %d\n", lrt_shmem_lock());
+    *maddr(0x60000008) = 2025;
+    read_value = *maddr(0x60000008);
+    uart_printf("Read test 2: %d\n", read_value);
+    uart_printf("exmem unlock = %d\n", lrt_shmem_unlock());
+
+    uart_printf("svar is in %x\n", &sharedvar);
 }
 
 // Task implementations
@@ -66,17 +97,17 @@ void init(void *arg)
     uint32_t currentid = lrt_task_current();
     uart_printf("Init task started. Task ID is %d, param is %x.\n", currentid, arg);
 
-    // lrt_signal_new(&signalid);
+    lrt_signal_new(&signalid);
 
-    lrt_task_start(mytask, 0, 1);
+    lrt_task_start(test_task1, 0, 1);
     lrt_task_prio(1, 2);
 
     uart_printf("Task 1 started.\n");
 
-    // lrt_task_start(mytask2, 2);
-    // lrt_task_prio(2, 1);
+    lrt_task_start(test_task2, 0, 2);
+    lrt_task_prio(2, 1);
 
-    // lrt_task_terminate(-1);
+    uart_printf("Task 2 started.\n");
 
     while (1)
         ;
@@ -109,33 +140,12 @@ int main(void)
     uint32_t ememsize = lrt_shmem_size();
     uart_printf("External shared memory controller reports: base %x size %x\n", emembase, ememsize);
 
-    /* *maddr(0x60000008) = 1958;
-    uint32_t read_value = *maddr(0x60000008);
-    uart_printf("Read test: %d\n", read_value);
-
-    uart_printf("exmem lock = %d\n", lrt_shmem_lock());
-    *maddr(0x60000008) = 2025;
-    read_value = *maddr(0x60000008);
-    uart_printf("Read test 2: %d\n", read_value);
-    uart_printf("exmem unlock = %d\n", lrt_shmem_unlock());
-
-    uart_printf("svar is in %x\n", &sharedvar); */
-
     systick_set_clocksource(STK_CSR_CLKSOURCE_AHB);
     systick_set_reload(7200000); // 10ms @72MHz
     systick_interrupt_enable();
     systick_counter_enable();
 
     uart_write("Initialization completed.\n");
-
-    /* while (1)
-    {
-        if (usart_get_flag(USART1, USART_FLAG_RXNE))
-        {
-            char c = usart_recv(USART1);        // Blocking read
-            usart_send_blocking(USART1, c + 1); // Echo it back
-        }
-    } */
 
     uart_write("Starting init task...\n");
 
